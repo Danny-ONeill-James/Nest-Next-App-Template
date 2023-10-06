dc0c5f035166f5d0e2baf4e65262410d61b3ccb7
Danny <116497508+Danny-ONeill-James@users.noreply.github.com>
2023-10-06 17:57:57 +0100

    feat: added API and Web build and start script

--- apps/web/app/trpc.ts
+++ apps/web/app/trpc.ts
@@ -4,7 +4,7 @@ import { createTRPCProxyClient, httpBatchLink } from "@trpc/client";
 export const trpc = createTRPCProxyClient<AppRouter>({
   links: [
     httpBatchLink({
-      url: `${process.env.NEXT_PUBLIC_API_URL}/trpc`, //TODO: use env var
+      url: `${process.env.NEXT_PUBLIC_API_URL}/trpc`,
     }),
   ],
 });
