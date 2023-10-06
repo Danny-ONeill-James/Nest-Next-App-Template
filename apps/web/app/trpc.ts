ebf1e7fa392d41a974a5194fb1efdef25cd064f0
Danny <116497508+Danny-ONeill-James@users.noreply.github.com>
2023-10-06 17:48:23 +0100

    feat: api port now from .env.local

--- apps/web/app/trpc.ts
+++ apps/web/app/trpc.ts
@@ -4,7 +4,7 @@ import { createTRPCProxyClient, httpBatchLink } from "@trpc/client";
 export const trpc = createTRPCProxyClient<AppRouter>({
   links: [
     httpBatchLink({
-      url: "http://localhost:4000/trpc", //TODO: use env var
+      url: `${process.env.NEXT_PUBLIC_API_URL}/trpc`, //TODO: use env var
     }),
   ],
 });
