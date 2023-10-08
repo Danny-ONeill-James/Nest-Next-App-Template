3e52df7d0e92c37cbfcb59d33b98c3e2ce79fb69
Danny O'Neill-James <116497508+Danny-ONeill-James@users.noreply.github.com>
2023-10-08 12:34:01 +0100

    Revert "Feat: initial build"

--- apps/api/src/trpc/trpc.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TrpcService } from './trpc.service';
-import { TrpcRouter } from './trpc.router';
-
-@Module({
-  imports: [],
-  providers: [TrpcService, TrpcRouter],
-})
-export class TrpcModule {}
