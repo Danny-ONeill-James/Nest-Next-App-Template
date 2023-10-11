1e4b91fc96e369385b9d7eda5560507c261fd34e
Danny O'Neill-James <116497508+Danny-ONeill-James@users.noreply.github.com>
2023-10-11 19:15:31 +0100

    feat: added db connection

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,13 +1,30 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TrpcModule } from './trpc/trpc.module';
 import { AuthModule } from './auth/auth.module';
+import { TrpcModule } from './trpc/trpc.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), TrpcModule, AuthModule, UsersModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      password: 'pass123',
+      username: 'postgres',
+      entities: [],
+      database: 'postgres',
+      synchronize: true,
+      logging: true,
+    }),
+    ConfigModule.forRoot(),
+    TrpcModule,
+    AuthModule,
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
