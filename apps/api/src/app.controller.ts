137ae9709f36a06adafdc3d9baf67d7b9fbc0a77
Danny O'Neill-James <116497508+Danny-ONeill-James@users.noreply.github.com>
2023-10-11 17:53:34 +0100

    Feat: Login route

--- apps/api/src/app.controller.ts
+++ apps/api/src/app.controller.ts
@@ -1,12 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  UseGuards,
+  Request as NestRequest,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 import { AppService } from './app.service';
+import { Request } from 'express'; // <-- Import express Request
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @UseGuards(AuthGuard('local'))
+  @Post('auth/login')
+  async login(@NestRequest() req: Request) {
+    return req.user;
   }
 }
