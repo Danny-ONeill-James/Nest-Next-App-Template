bef2324f56870d097505447ae1511d24e530a909
Danny O'Neill-James <116497508+Danny-ONeill-James@users.noreply.github.com>
2023-10-11 16:36:48 +0100

    Revert "Revert "Feat: initial build""

---
+++ apps/web/app/trpc.ts
@@ -0,0 +1,10 @@
+import type { AppRouter } from "@api/trpc/trpc.router";
+import { createTRPCProxyClient, httpBatchLink } from "@trpc/client";
+
+export const trpc = createTRPCProxyClient<AppRouter>({
+  links: [
+    httpBatchLink({
+      url: `${process.env.NEXT_PUBLIC_API_URL}/trpc`,
+    }),
+  ],
+});
